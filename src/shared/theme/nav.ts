0ba0dcaf54fd48aed98fddfe637804792e81d746
Dave Mackintosh <me@davemackintosh.co.uk>
2019-08-24 09:44:31 +0100

    Fixed broken navugation with booking button (#16)

--- src/shared/theme/nav.ts
+++ src/shared/theme/nav.ts
@@ -1,6 +1,7 @@
 import { Grid } from "@src/shared/theme/grid"
 import { NavLink as RRNavLink } from "react-router-dom"
 import styled from "styled-components"
+import media from "styled-media-query"
 
 export const Nav = styled.nav`
   width: 80vw;
@@ -10,10 +11,20 @@ export const Nav = styled.nav`
 export const NavUl = styled(Grid("ul"))`
   margin: 0;
   padding: 0;
+
+  ${media.lessThan("medium")`
+    grid-template-columns: 1fr 1fr;
+  `}
 `
 
 export const NavLi = styled.li`
   list-style: none;
+
+  ${media.lessThan("medium")`
+    &:nth-child(3) {
+      grid-column: span 2;
+    }
+  `}
 `
 
 export const SkipToContentNavLink = styled.a`
