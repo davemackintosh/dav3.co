f955cd0081a16b405a33e01d662b839d66b94dba
davemackintosh <me@davemackintosh.co.uk>
2019-10-07 16:38:18 +0100

    Fixes to some styles and small post fixes to include published date and time to read

--- src/shared/components/markdown-renderers/link.tsx
+++ src/shared/components/markdown-renderers/link.tsx
@@ -5,6 +5,17 @@ type AToLinkProps = LinkProps & HTMLAnchorElement
 
 class AToLink extends PureComponent<AToLinkProps> {
   public render(): JSX.Element {
+    if (this.props.href.startsWith("http"))
+      return (
+        <a
+          href={this.props.href}
+          title={this.props.title}
+          rel="noopener noreferrer"
+        >
+          {this.props.children}
+        </a>
+      )
+
     return (
       <Link to={this.props.href} title={this.props.title}>
         {this.props.children}
