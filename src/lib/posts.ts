695b614d9cef13fa0cc7ad2d5ea75757950a8c3e
davemackintosh <me@davemackintosh.co.uk>
2024-01-02 09:20:37 +0000

    Adding post for my dotfiles.

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -8,7 +8,7 @@ export interface PostMeta {
 	author?: string
 	excerpt?: string
 	keywords?: string[]
-	published?: Date
+	published: Date
 	title: string
 	// A Svelte component as rendered by mdsvex
 	content: ComponentType
@@ -20,25 +20,25 @@ export interface PostWithContent {
 }
 
 export const fetchMarkdownPosts = async (): Promise<PostWithContent[]> => {
-	const allPostFiles = import.meta.glob<GlobbedPosts>('/src/routes/blog/*.md')
+	const allPostFiles = import.meta.glob<GlobbedPosts>("/src/routes/blog/*.md")
 	const iterablePostFiles = Object.entries(allPostFiles)
 
-	const allPosts = await Promise.all(
-		iterablePostFiles.map(async ([path, resolver]) => {
-			const data = await resolver()
-			const { metadata } = data
-			const postPath = path.slice(11, -3)
+	const allPosts: PostWithContent[] = (
+		await Promise.all(
+			iterablePostFiles.map(async ([path, resolver]): Promise<PostWithContent> => {
+				const data = await resolver()
+				const { metadata } = data
+				const postPath = path.slice(11, -3)
 
-			if (metadata.published)
-				metadata.published = new Date(metadata.published)
+				if (metadata.published) metadata.published = new Date(metadata.published)
 
-			return {
-				meta: metadata,
-				path: postPath,
-			}
-		})
-	)
+				return {
+					meta: metadata,
+					path: postPath,
+				}
+			}),
+		)
+	).sort((a, b) => b.meta.published.valueOf() - a.meta.published.valueOf())
 
 	return allPosts
 }
-
