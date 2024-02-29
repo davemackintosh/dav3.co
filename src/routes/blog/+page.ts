418edad00815014196025177fb7af2cb55debbd8
Dave Mackintosh <me@davemackintosh.co.uk>
2024-02-29 21:49:02 +0000

    Revert "Make the work history section dynamic and file based"
    This reverts commit 1d269c742e9fc8ef29e7c2428990b093112715f9.

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,10 +1,9 @@
-import { fetchMarkdownPosts, type PostMeta } from "$lib/posts"
-import type { MarkdownContent } from "$src/lib/markdown"
-export const prerender = true
-export const load = async (): Promise<{ posts: MarkdownContent<PostMeta>[] }> => {
+import { fetchMarkdownPosts } from "$lib/posts"
+export const prerender = true;
+export const load = async () => {
 	const posts = await fetchMarkdownPosts()
 
 	return {
-		posts,
+		posts
 	}
 }
