386a8316ea8de46efb1ec7dcc8409b0b863dd705
davemackintosh <me@davemackintosh.co.uk>
2024-05-20 22:08:11 +0100

    Enabled speed insights.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { inject } from "@vercel/analytics"
+	import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
 	import "@fontsource/ibm-plex-mono"
 	import "$src/styles.css"
 
 	inject()
+	injectSpeedInsights()
 </script>
 
 <slot />
